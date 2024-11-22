318e2be6feacbd7bf163db763e187271822cef19
Abdulmuqsit Jimoh <135670967+abdulmuqsit619@users.noreply.github.com>
2024-11-22 14:36:55 -0500

    Update driverfile.c

--- driverfile.c
+++ driverfile.c
@@ -23,4 +23,54 @@ static void reset_sequence(void) {
     printk(KERN_INFO "Sequence reset.\n");
 }
 
-static int keyboard_event_notify
+static int keyboard_event_notify(struct notifier_block *nb, unsigned long action, void *data) {
+    struct keyboard_notifier_param *param = data;
+    unsigned long current_time = jiffies;
+
+    if (action == KBD_KEYCODE && param->down) {
+        char *key_name;
+
+        switch (param->value) {
+            case KEY_LEFTCTRL: key_name = "ctrl"; break;
+            case KEY_LEFTALT:  key_name = "alt"; break;
+            case KEY_Y:        key_name = "y"; break;
+            default: reset_sequence(); return NOTIFY_OK;
+        }
+
+        if (time_after(jiffies, last_key_time + msecs_to_jiffies(timeout_ms))) {
+            reset_sequence();
+        }
+        last_key_time = current_time;
+
+        if (strcmp(key_name, sequence[seq_index]) == 0) {
+            seq_index++;
+            if (seq_index == ARRAY_SIZE(sequence)) {
+                printk(KERN_INFO "Shortcut detected: Ctrl + Alt + Y\n");
+                execute_action();
+                reset_sequence();
+            }
+        } else {
+            reset_sequence();
+        }
+    }
+
+    return NOTIFY_OK;
+}
+
+static struct notifier_block keyboard_nb = {
+    .notifier_call = keyboard_event_notify
+};
+
+static int __init shortcut_driver_init(void) {
+    printk(KERN_INFO "Loading shortcut driver...\n");
+    register_keyboard_notifier(&keyboard_nb);
+    return 0;
+}
+
+static void __exit shortcut_driver_exit(void) {
+    printk(KERN_INFO "Unloading shortcut driver...\n");
+    unregister_keyboard_notifier(&keyboard_nb);
+}
+
+module_init(shortcut_driver_init);
+module_exit(shortcut_driver_exit);
